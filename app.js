362e0c4c542c991b6d570231682d1650c72265a4
Zachariah Nagatani <zachnagatani@gmail.com>
2017-02-09 13:59:51 -0800

    Feat: Form Handling and Response

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express'),
       app = express(),
+      fs = require('fs'),
+      multer = require('multer'),
+      upload = multer({dest: 'uploads/'}),
       port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/public'));
@@ -9,6 +12,18 @@ app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/get-file-size', (req, res) => {
+    res.json({
+        size: req.query.size + ' bytes'
+    });
+});
+
+app.post('/get-file-size', upload.single('file'), (req, res) => {
+    res.json({
+        size: req.file.size + ' bytes'
+    });
 });
 
 app.listen(port);
